ae9ee31a4bf15aa8b7064c4e87e8084e26b4effe
PineappleLB <31363497+PineappleLB@users.noreply.github.com>
2019-01-26 21:34:08 +0800

    增加查询按钮

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
-    <div>
-      <el-autocomplete
-        class="inline-input"
-        v-model="keyWords"
-        :fetch-suggestions="querySearch"
-        placeholder="请输入内容"
-        @select="handleSelect"
-      ></el-autocomplete>
+    <div id="search">
+      <el-row>
+        <el-col :span="24">
+          <el-autocomplete
+            class="inline-input"
+            v-model="keyWords"
+            @change="changeKeyWords(this)"
+            :fetch-suggestions="querySearch"
+            placeholder="请输入内容"
+            @select="handleSelect"
+          >
+            <el-button slot="append" icon="el-icon-search" @click="handleSearch()"></el-button>
+          </el-autocomplete>
+        </el-col>
+      </el-row>
     </div>
     <div id="content"></div>
   </div>
@@ -22,27 +29,37 @@ export default {
     };
   },
   methods: {
-    handleSelect(item) {
+    handleSearch(){
+      this.search(this.keyWords);
+    },
+    search(val){
       this.$.ajax({
         type: "get",
         async: true,
-        url: "/api/s?&wd=" + encodeURI(item.value),
+        url: "/api/s?&wd=" + encodeURI(val),
         dataType: "html",
         success: function(data) {
-          let left_content = $(data).find("#container #content_left .result h3");
+          let left_content = $(data).find(
+            "#container #content_left .result h3"
+          );
           document.getElementById("content").innerHTML = "";
-          for(let i in left_content){
+          for (let i in left_content) {
             document.getElementById("content").appendChild(left_content[i]);
           }
           // left_content[0].innerHTML;
           var str = JSON.stringify(data);
         },
         error: function(e) {
-          console.error(e)
+          console.error(e);
         }
       });
     },
+    handleSelect(item) {
+      this.search(item.value)
+    },
     querySearch(queryString, cb) {
+      console.log(queryString);
+      this.keyWords = queryString;
       this.$jsonp(
         `https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/?wd=${queryString}`,
         { param: "cb" },
@@ -72,15 +89,28 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 800px;
+  margin: 0 auto;
   margin-top: 60px;
 }
 .el-input__inner {
   text-align: center;
 }
+.el-autocomplete{
+  width: 100%;
+}
 .el-autocomplete-suggestion li {
   text-align: center;
 }
-#content .t em{
+#search {
+  width: 600px;
+  margin: 0 auto;
+}
+
+#content .t em {
   color: red;
+}
+#content {
+  width: 800px;
 }
 </style>
